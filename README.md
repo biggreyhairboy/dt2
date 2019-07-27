d3f09774e6193e1676552a451670b06968d4977b
patrick <biggreyhairboy@gmail.com>
2019-07-27 09:23:06 +0000

    主体结构搭好，需要填充内容

---
+++ README.md
@@ -0,0 +1,5 @@
+** 主要内容是读取配置，启动数据记录类
+** child and parent process 可以从1.0版本里面参考
+** 应该还是需要app的概念，不过是没有ui的app
+** engine是最底层的结构
+
---
+++ dt2.py
@@ -0,0 +1,28 @@
+from datatime import datatime
+
+
+from vnpy.event import Event,EventEngine
+from vnpy.trader engine import MainEngine 
+from vnpy.trader.event import EVENT_CONTRACT
+
+from engine import(
+        APP_NAME,
+        EVENT_RECORDER_LOG,
+        EVENT_RECORDER_UPDATE
+        )
+
+class DataRecorderManager()
+    def __init__(self, engine:MainEngine, event_engine:EventEngine):
+        super.__init__()
+
+        self.main_engine = engine
+        self.evnet_engine = event_engine
+        self.recoder_engine = main_engine.get_engine(APP_NAME)
+        //todo 需要读取配置并将没给symbol和tick的读取配置注册到事件中
+        self.init_setting()
+        self.register_event()
+        self.recoder_engine.put_event()
+    
+    def init_setting()
+        pass
+
